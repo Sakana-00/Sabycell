c0faa7e843b626d10c65a5f360250a11af316ad8
Sakana-00 <sakanakaah@gmail.com>
2025-12-02 15:28:32 +0700

    mengubah direktori

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-    plugins: [react()],
-    // Set base path conditionally.
-    // On 'build', set it to the repository name for GitHub Pages.
-    // On 'serve' (npm run dev), keep it as root '/'.
-    base: command === 'build' ? '/sabycell/' : '/',
+  plugins: [react()],
+  base: command === 'build' ? '/sabycell/' : '/',
 }));
